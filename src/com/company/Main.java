0e2021cb484bc3364f01c1883c2ca3da0ea97783
dns <pochti_zvezda@mail.ru>
2022-01-25 21:37:16 +0300

    dz15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,81 @@
 package com.company;
 
+import java.util.Arrays;
+
+import static com.company.IntListImpl.sortMyArray;
+
 public class Main {
 
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public static void sortBubble(int[]arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+                }
+            }
+        }
+    }
+    public static void sortSelection(int[]arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+    private static void sortInsertion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+
+        IntListImpl data = new IntListImpl(10);
+
+        //выявление самой быстрой сортировки
+
+        int[] copy1 = Arrays.copyOf(generateRandomArray(), generateRandomArray().length);
+        int[] copy2 = Arrays.copyOf(generateRandomArray(), generateRandomArray().length);
+        int[] copy3 = Arrays.copyOf(generateRandomArray(), generateRandomArray().length);
+
+
+        long start = System.currentTimeMillis();
+        sortBubble(copy1);
+        System.out.println(System.currentTimeMillis() - start);
+
+        long start2 = System.currentTimeMillis();
+        sortSelection(copy2);
+        System.out.println(System.currentTimeMillis() - start2);
+
+        long start3 = System.currentTimeMillis();
+        sortInsertion(copy3);
+        System.out.println(System.currentTimeMillis() - start3);
+
     }
 }
